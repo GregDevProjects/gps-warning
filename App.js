522ec4a4753c6bd02b8512234ed81e5f3f9d66d2
GregDevProjects <gdonaldsonmclean@gmail.com>
2021-05-05 20:54:26 -0300

    add map, geofence, setup env variables

--- App.js
+++ App.js
@@ -1,58 +1,47 @@
+//https://github.com/react-native-maps/react-native-maps/blob/master/docs/installation.md MAP INSTALL
 import React, { useState, useEffect } from "react";
 import { Platform, Text, View, StyleSheet, Button } from "react-native";
 import * as Location from "expo-location";
 import isLocationPermissionGiven from "./src/isLocationPermissionGiven";
-
+import regions from "./test_data/regions";
 import * as TaskManager from "expo-task-manager";
 import { LocationGeofencingEventType } from "expo-location";
+import MapView from "react-native-maps";
+import { Circle } from "react-native-maps";
+
+//import Config from "react-native-config";
+//console.log("KEY", Config);
+
 const TASK_NAME = "IN_GEOFENCE";
 
 TaskManager.defineTask(TASK_NAME, ({ data: { eventType, region }, error }) => {
-  console.log("task manager invoked");
   if (error) {
-    console.error(error);
+    console.error(error.message);
     // check `error.message` for more details.
     return;
   }
 
-  console.log(eventType);
+  // console.log(region);
 
   if (eventType === LocationGeofencingEventType.Enter) {
-    alert("You've entered region:", eventType.identifier);
+    alert(`You've entered region: ${region.identifier}`);
   } else if (eventType === LocationGeofencingEventType.Exit) {
-    alert("You've left region:", eventType.identifier);
+    alert(`You've left region: ${region.identifier}`);
   }
 });
-
-const regions = [
-  {
-    identifier: "test_identifier",
-    latitude: 44.6683, //44.66821413533673,
-    longitude: -63.6074, //-63.6072027683258,
-    radius: 1000,
-    notifyOnEnter: true,
-    notifyOnExit: true,
-  },
-  {
-    identifier: "home",
-    latitude: 44.6618291, //44.66821413533673,
-    longitude: -63.6005524, //-63.6072027683258,
-    radius: 1000,
-    notifyOnEnter: true,
-    notifyOnExit: true,
-  },
-];
 
 export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
 
   const [buttonText, setButtonText] = useState("Check geofencing status");
+  const [tasks, setTasks] = useState("Tasks displayed here");
+  const [initialPosition, setInitialPosition] = useState(undefined);
 
   useEffect(() => {
     (async () => {
       let permissionsGiven = await isLocationPermissionGiven();
-
+      let tasks = undefined;
       if (!permissionsGiven) {
         setErrorMsg("Permission not granted!");
         return;
@@ -65,10 +54,18 @@ export default function App() {
       if (isTaskManagerEnabled) {
         console.log("Task manager enabled, starting geofence");
         Location.startGeofencingAsync(TASK_NAME, regions);
-      }
 
-      // let location = await Location.startLocationUpdatesAsync("LOCATION_CHECK");
-      // setLocation(location);
+        const location = await Location.getCurrentPositionAsync();
+
+        setInitialPosition({
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.006,
+          longitudeDelta: 0.006,
+        });
+
+        console.log("Got location", location);
+      }
     })();
   }, []);
 
@@ -79,11 +76,19 @@ export default function App() {
     text = "Permissions given";
   }
 
-  const styles = StyleSheet.create({ container: {}, paragraph: {} });
+  const styles = StyleSheet.create({
+    container: {},
+    paragraph: {},
+    map: {
+      width: 300,
+      height: 500,
+    },
+  });
 
   return (
     <View style={styles.container}>
       <Text style={styles.paragraph}>{text}</Text>
+      <Text style={styles.paragraph}>{tasks}</Text>
       <Button
         onPress={async () => {
           const isGeoFencing = await Location.hasStartedGeofencingAsync(
@@ -92,14 +97,34 @@ export default function App() {
 
           if (isGeoFencing) {
             setButtonText("Working");
+
+            const tasks = await TaskManager.getRegisteredTasksAsync();
+            setTasks(JSON.stringify(tasks));
             return;
           }
+
           setButtonText("Error");
         }}
         title={buttonText}
         color="#841584"
         accessibilityLabel="Learn more about this purple button"
       />
+      <MapView
+        initialRegion={initialPosition}
+        showsUserLocation={true}
+        style={styles.map}
+      >
+        {regions.map((region, index) => (
+          <Circle
+            key={index}
+            center={{
+              latitude: region.latitude,
+              longitude: region.longitude,
+            }}
+            radius={region.radius}
+          ></Circle>
+        ))}
+      </MapView>
     </View>
   );
 }
---
+++ test_data/regions.js
@@ -0,0 +1,36 @@
+const regions = [
+  {
+    identifier: "home",
+    latitude: 44.6618, //44.66821413533673,
+    longitude: -63.6006, //-63.6072027683258,
+    radius: 20,
+    notifyOnEnter: true,
+    notifyOnExit: true,
+  },
+  {
+    identifier: "Mike and Adrians",
+    latitude: 44.6521, //44.66821413533673,
+    longitude: -63.5984, //-63.6072027683258,
+    radius: 100,
+    notifyOnEnter: true,
+    notifyOnExit: true,
+  },
+  {
+    identifier: "Fort Needham",
+    latitude: 44.664814, //44.66821413533673,
+    longitude: -63.600442, //-63.6072027683258,
+    radius: 225,
+    notifyOnEnter: true,
+    notifyOnExit: true,
+  },
+  {
+    identifier: "Kempt - Young corner",
+    latitude: 44.659173, //44.66821413533673,
+    longitude: -63.603058, //-63.6072027683258,
+    radius: 100,
+    notifyOnEnter: true,
+    notifyOnExit: true,
+  },
+];
+
+export default regions;
